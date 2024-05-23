1b316727fb91ea1804b684936469ae18aac506ec
Leo Feng <thcheetah777@gmail.com>
2024-05-23 16:12:34 -0500

    💖 add favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   import type { PageData } from "./$types";
-  import { currentTheme } from "$lib/stores";
 
   import TodoList from "$components/TodoList.svelte";
   import ColorPicker from "$components/ColorPicker.svelte";
-	import Button from "$components/Button.svelte";
 
   export let data: PageData;
-  let { supabase } = data;
   $: ({ supabase } = data)
+  let { supabase } = data;
 
   async function logOut() {
     await supabase.auth.signOut();
@@ -16,11 +14,7 @@
   }
 </script>
 
-<div class="p-8 flex justify-evenly items-center h-full w-full">
-  <Button borderWidth="3px" shadowSize="6px" class="rounded-2xl p-3" color={$currentTheme}>
-    <img src="/sozo.svg" alt="Sozo Logo" class="invert">
-  </Button>
-
+<main class="p-8 flex justify-evenly items-center h-full">
   <TodoList />
   <ColorPicker />
-</div>
+</main>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,1 +1,15 @@
-<h1>Landing Page</h1>
+<script lang="ts">
+  import Button from "$components/Button.svelte";
+  import { currentTheme } from "$lib/stores";
+</script>
+
+<main class="p-6 flex flex-col items-center h-full gap-4">
+  <Button
+    borderWidth="3px"
+    shadowSize="6px"
+    color={$currentTheme}
+    class="rounded-2xl p-3"
+  >
+    <img src="/logo.svg" alt="Sozo Logo">
+  </Button>
+</main>
